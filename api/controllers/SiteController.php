b125d0eaa0ab860dce2362c565a3b651e14e3e34
Nishant Bhatt <nishant.b@befree.com.au>
2017-08-02 17:50:25 +0530

    api setup

---
+++ api/controllers/SiteController.php
@@ -0,0 +1,50 @@
+<?php
+
+    namespace api\controllers;
+
+    use Yii;
+    use yii\base\Object;
+    use yii\web\Controller;
+    use yii\web\Response;
+
+    class SiteController extends Controller
+    {
+        
+        /**
+        * Displays homepage.
+        *
+        * @return mixed
+        */
+       public function actionIndex()
+       {
+           $this->layout ="ajax";
+           return $this->render('index');
+       }
+
+        public function actionPing()
+        {
+            $response = new Response();
+            $response->statusCode = 200;
+            $response->data = Yii::t('app','pong');
+
+            return $response;
+        }
+
+
+        public function actionError() {
+
+            $response = new Response();
+            $response->statusCode = 400;
+            $response->data = json_encode([
+                "name"      => "Bad Request",
+                "message"   => Yii::t('app', 'The system could not process your request. Please check and try again.'),
+                "code"      => 0,
+                "status"    => 400,
+                "type"      => "yii\\web\\BadRequestHttpException"
+            ]);
+
+            return $response;
+        }
+
+
+    }
