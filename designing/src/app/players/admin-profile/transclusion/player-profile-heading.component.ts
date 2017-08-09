87a341c13995a37afc69b3216308deeec06956db
Hemadri Patel <hemadri.p@befree.com.au>
2017-08-09 16:45:27 +0530

    remove files

--- designing/src/app/players/admin-profile/transclusion/player-profile-heading.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'profile-heading',
-    templateUrl: 'player-profile-heading.component.html'
-})
-export class PlayerProfileHeadingComponent { }
--- frontend/src/app/forgot-password/forgot-password.ts
+++
@@ -1,3 +0,0 @@
-/**
- * Created by pankajk on 8/1/2017.
- */
